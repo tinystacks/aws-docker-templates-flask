aa1d3f48983c9d6b9e4d0a78693ac4cd8a54b96e
FrancescoXX <francescociullaroma@gmail.com>
2021-11-16 19:02:30 +0100

    local-postgres-flask

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,11 @@
-# FROM python:3.6
-FROM public.ecr.aws/bitnami/python:3.6
+FROM python:3.6-slim-buster
 
-# Create app directory
-WORKDIR /app
-
-# Copy the requirements file
 COPY requirements.txt .
 
-# Install app dependencies
 RUN pip install -r requirements.txt
 
-# start the virtual environment
-RUN python3 -m venv venv
-
-# Copy the whole folder inside the Image filesystem
 COPY . .
 
-EXPOSE 80
+# EXPOSE 80
 
-CMD gunicorn --bind 0.0.0.0:80 wsgi:app
+CMD ["flask", "run", "--host=0.0.0.0", "--port=80"]
--- app.py
+++ app.py
@@ -1,159 +1,57 @@
-from flask import Flask, request, Response, jsonify
-import boto3
-import os
-import datetime
-import uuid
-import json
-from boto3.dynamodb.conditions import Key
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import os
 
-# App Initialization
 app = Flask(__name__)
 
-app.config.from_pyfile('./config/appconfig.cfg')
-CONF = f"postgresql://{app.config['PG_USER']}:{app.config['PG_PASSWORD']}@{app.config['PG_HOST']}:{app.config['PG_PORT']}/{app.config ['PG_DATABASE']}"
-app.config['SQLALCHEMY_DATABASE_URI'] = CONF
+if __name__ == '__main__':
+    app.run(debug=True)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 
-# Model Class for Postgres integration
 class Item(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), unique=True, nullable=False)
-    content = db.Column(db.String(200), nullable=False)
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(80), unique=True, nullable=False)
+  content = db.Column(db.String(120), unique=True, nullable=False)
 
-    def __init__(self, title, content):
-        self.title = title
-        self.content = content
+  def __init__(self, title, content):
+    self.title = title
+    self.content = content
 
-# DynamoDB
-dynamodb_client = None
-table = None
+db.create_all()
 
-def dynamodb_table():
-  global dynamodb_client
-  global table
-  if (dynamodb_client == None):
-    dynamodb_client = boto3.resource('dynamodb')
-    table = dynamodb_client.Table(os.environ.get('TABLE_NAME', 'TableNameEnvVarNotSet'))
-  return table
+@app.route('/items/<id>', methods=['GET'])
+def get_item(id):
+  item = Item.query.get(id)
+  del item.__dict__['_sa_instance_state']
+  return jsonify(item.__dict__)
 
-def cognito_client():
-  if (cognito == none):
-    cognito = boto3.client('cognito-idp')
-  return cognito
+@app.route('/items', methods=['GET'])
+def get_items():
+  items = []
+  for item in db.session.query(Item).all():
+    del item.__dict__['_sa_instance_state']
+    items.append(item.__dict__)
+  return jsonify(items)
 
-local_items = []
+@app.route('/items', methods=['POST'])
+def create_item():
+  body = request.get_json()
+  db.session.add(Item(body['title'], body['content']))
+  db.session.commit()
+  return "item created"
 
-@app.route('/', methods=['GET'])
-def get():
-    return ""
+@app.route('/items/<id>', methods=['PUT'])
+def update_item(id):
+  body = request.get_json()
+  db.session.query(Item).filter_by(id=id).update(
+    dict(title=body['title'], content=body['content']))
+  db.session.commit()
+  return "item updated"
 
-@app.route('/ping', methods=['GET'])
-def ping():
-    return ""
-
-@app.route('/postgres-item', methods=['GET'])
-def itemget():
-    query = '''CREATE TABLE if not exists item(id serial PRIMARY KEY, title VARCHAR (200) UNIQUE NOT NULL, content VARCHAR (200) NOT NULL);'''
-    db.engine.execute(query)
-
-    items = []
-    for item in db.session.query(Item).all():
-        del item.__dict__['_sa_instance_state']
-        items.append(item.__dict__)
-
-    return jsonify(items)
-
-@app.route('/postgres-item', methods=['POST'])
-def itemadd():
-    query = '''CREATE TABLE if not exists item(id serial PRIMARY KEY, title VARCHAR (200) UNIQUE NOT NULL, content VARCHAR (200) NOT NULL);'''
-    db.engine.execute(query)
-    
-    request_data = request.get_json()
-    title = request_data["title"]
-    content = request_data["content"]
-    
-    entry = Item(title, content)
-    db.session.add(entry)
-    db.session.commit()
-    
-    return jsonify("item created")
-
-@app.route('/postgres-item', methods=['PUT'])
-def update_item():
-    return "update_item"
-
-@app.route('/postgres-item', methods=['DELETE'])
-def delete_item():
-    return "delete_item"
-
-@app.route('/item', methods=['PUT'])
-def put_item():
-    request_data = request.get_json()
-    print(request_data['title'])
-    item = {'title': request_data['title'], 'content': request_data['content']}
-    local_items.append(item)
-    return make_response("")
-    
-@app.route('/item', methods=['GET'])
-def get_item():
-    return make_response(json.dumps(local_items))
-
-@app.route('/db-item', methods=['PUT'])
-def put_db_item():
-    request_data = request.get_json()
-    dynamodb_table().put_item(
-        Item={
-            'userId': "SYSTEM",
-            'itemId': str(uuid.uuid4()),
-            'title': request_data['title'],
-            'content': request_data['content']
-        }
-    )
-    return make_response("")
-    
-@app.route('/db-item', methods=['GET'])
-def get_db_items():
-    return make_response(str(dynamodb_table().scan()['Items']))
-
-@app.route('/db-item', methods=['DELETE'])
-def delete_db_item():
-    request_data = request.get_json()
-    dynamodb_table().delete_item(
-        Key={
-            'userId': "SYSTEM",
-            'itemId': str(uuid.uuid4()),
-        }
-    )
-    return make_response("")
-
-@app.route('/authenticated-item', methods=['PUT'])
-def put_authenticated():
-    request_data = request.get_json()
-    user = cognito_client.get_user(AccessToken=request.headers['authorization'].split(' ')[1])
-    dynamodb_table().put_item(
-        Item={
-            'userId': user['Username'],
-            'itemId': str(uuid.uuid4()),
-            'title': request_data['title'],
-            'content': request_data['content']
-        }
-    )
-    return make_response("")
-
-@app.route('/authenticated-item', methods=['GET'])
-def get_authenticated():
-    user = cognito_client.get_user(AccessToken=request.headers['authorization'].split(' ')[1])
-    results = dynamodb_table().query(
-        KeyConditionExpression=Key('userId').eq(user['Username'])
-    )
-    return make_response(str(results["Items"]))
-
-def make_response(rv):
-    resp = Response(rv)
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    resp.headers['Access-Control-Allow-Credentials'] = 'true'
-    return resp
+@app.route('/items/<id>', methods=['DELETE'])
+def delete_item(id):
+  db.session.query(Item).filter_by(id=id).delete()
+  db.session.commit()
+  return "item deleted"
